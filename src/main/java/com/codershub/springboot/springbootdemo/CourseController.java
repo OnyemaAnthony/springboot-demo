21e4b38083e5df3f36f82d00a194a285f3ffc070
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-06-23 16:56:47 +0100

    create get REST

---
+++ src/main/java/com/codershub/springboot/springbootdemo/CourseController.java
@@ -0,0 +1,19 @@
+package com.codershub.springboot.springbootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Learn AWS","Anthony"),
+                new Course(1,"Learn java","Anthony")
+        );
+    }
+}
